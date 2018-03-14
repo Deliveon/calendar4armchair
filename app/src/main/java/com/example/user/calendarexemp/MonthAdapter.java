d45b15236969d80baa019f1c437279b0fcbcb0b4
TRAD3R <6292676@gmail.com>
2018-03-14 21:18:16 +0300

    commit

--- app/src/main/java/com/example/user/calendarexemp/MonthAdapter.java
+++ app/src/main/java/com/example/user/calendarexemp/MonthAdapter.java
@@ -7,19 +7,15 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import java.util.Arrays;
 
-/**
- * Created by User on 01.02.2018.
- */
-
-public class Adapter extends BaseAdapter {
+//Адаптер для описания месяца по дням
+public class MonthAdapter extends BaseAdapter {
     private Context context;
     String[] data = new String[42];
     String[] data1 = new String[42];
     String[] data2 = new String[42];
 
-    public Adapter(Context context, String[] data, String[] data1, String[] data2) {
+    public MonthAdapter(Context context, String[] data, String[] data1, String[] data2) {
         this.context = context;
         this.data = data;
         this.data1 = data1;
@@ -42,18 +38,19 @@ public class Adapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View grid;
-        grid = new View(context);
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View gridItem;
+        gridItem = new View(context);
         //LayoutInflater inflater = getLayoutInflater();
         LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
-        grid = inflater.inflate(R.layout.day, parent, false);
-        TextView textView = (TextView) grid.findViewById(R.id.textView);
-        TextView textView2 = (TextView) grid.findViewById(R.id.textView2);
-        TextView textView3 = (TextView) grid.findViewById(R.id.textView3);
+        gridItem = inflater.inflate(R.layout.day, parent, false);
+        TextView textView = (TextView) gridItem.findViewById(R.id.textView);
+        TextView textView2 = (TextView) gridItem.findViewById(R.id.textView2);
+        TextView textView3 = (TextView) gridItem.findViewById(R.id.textView3);
         textView.setText(data[position]);
         textView2.setText(data1[position]);
         textView3.setText(data2[position]);
-        return grid;
+
+        return gridItem;
     }
 }
